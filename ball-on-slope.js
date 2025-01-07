d5bacbbbe6443832c1b2f42d3a4aabcc35be0d2b
LeynaM <leynamay@gmail.com>
2025-01-07 15:25:38 +0000

    It bounces!!

--- ball-on-slope.js
+++ ball-on-slope.js
@@ -4,21 +4,20 @@ let ball;
 
 function setup() {
   createCanvas(1000, 1000);
-  gravity = createVector(0, 0.02);
+  gravity = createVector(0, 0.2);
 
   slope = {
     start: createVector(0, height * 0.5),
     end: createVector(width, height * 0.7),
   };
-  slope.gradient =
-    (slope.end.y - slope.start.y) / (slope.end.x - slope.start.x);
+  slope.gradient = (slope.end.y - slope.start.y) / (slope.end.x - slope.start.x);
   slope.angle = atan(slope.end.x / slope.start.y);
 
   ball = new RigidBody({
-    position: createVector(50, 360),
+    position: createVector(50, 200),
     velocity: createVector(0, 0),
     acceleration: createVector(0, 0),
-    radius: 0.05 * height,
+    radius: 0.03 * height,
   });
   ball.log();
 }
@@ -30,19 +29,17 @@ function draw() {
 
   const normalVector = findNormalVector();
   if (isColliding(normalVector)) {
+    ball.log();
     ball.bounce(normalVector);
-    noLoop();
+    ball.log();
+    // noLoop();
   }
   ball.update();
   ball.draw();
 
   stroke("blue");
   strokeWeight(2);
-  // const normalVector = findNormalVector();
-  console.log("ball", ball.position.toString());
-  console.log("nv", normalVector.toString());
   const intersect = ball.position.copy().sub(normalVector);
-  console.log("intersect", intersect.toString());
   drawVector(intersect, normalVector);
 }
 
@@ -50,16 +47,7 @@ function drawScene() {
   background("lightblue");
   noStroke();
   fill("green");
-  quad(
-    slope.start.x,
-    slope.start.y,
-    slope.end.x,
-    slope.end.y,
-    width,
-    height,
-    0,
-    height
-  );
+  quad(slope.start.x, slope.start.y, slope.end.x, slope.end.y, width, height, 0, height);
 }
 
 function findNormalVector() {
@@ -68,20 +56,14 @@ function findNormalVector() {
       slope.gradient ** 2 * slope.start.x +
       slope.gradient * (ball.position.y - slope.start.y)) /
     (slope.gradient ** 2 + 1);
-  const yIntersect =
-    slope.gradient * xIntersect -
-    slope.gradient * slope.start.x +
-    slope.start.y;
+  const yIntersect = slope.gradient * xIntersect - slope.gradient * slope.start.x + slope.start.y;
 
   const intersect = createVector(xIntersect, yIntersect);
-  console.log("interesectA", intersect.toString());
 
   return ball.position.copy().sub(intersect);
 }
 
 function isColliding(normalVector) {
-  // const normalVector = findNormalVector();
-
   return normalVector.mag() < ball.radius;
 }
 
@@ -91,14 +73,7 @@ function arrowHead(start, vector) {
   var norm = createVector(vector.x, vector.y);
   norm.normalize();
   // applyMatrix(1,0,0,1,vector.x - start.x,vector.y - start.y)
-  applyMatrix(
-    norm.x,
-    norm.y,
-    -norm.y,
-    norm.x,
-    vector.x - start.x,
-    vector.y - start.y
-  );
+  applyMatrix(norm.x, norm.y, -norm.y, norm.x, vector.x - start.x, vector.y - start.y);
 
   fill("blue");
   triangle(0, 60, 120, 0, 0, -60);
@@ -110,12 +85,6 @@ function drawVector(start, vector) {
   line(start.x, start.y, end.x, end.y);
   arrowHead(start, vector);
 }
-
-// function normalForce() {
-//   const magnitude = sqrt(ball.acceleration.x ** 2 + ball.acceleration.y ** 2) * sin(slope.angle);
-
-//   return createVector(magnitude * cos(slope.angle), -magnitude * sin(slope.angle));
-// }
 
 class RigidBody {
   constructor(initialProperties) {
@@ -162,14 +131,19 @@ class RigidBody {
   }
 
   bounce(normalVector) {
-    const unitNormal = normalVector.copy().normalize();
-    const v = 2 * this.velocity.copy().dot(normalVector);
-    console.log("\nbounce");
-    console.log(normalVector.toString());
-    console.log(unitNormal.toString());
-    console.log(v);
-    console.log(unitNormal.copy().mult(v).toString());
-    console.log("\n");
-    this.velocity.sub(unitNormal.mult(v));
+    // V as the initial velocity vector of the ball.
+    // V∥​ as the component of V parallel to the slope.
+    // V⊥​ as the component of V perpendicular to the slope.
+    // V′ as the resultant velocity vector of the ball.
+
+    // V∥ = (( V · n ) / |n|^2 ) * n
+    // V⊥ = V - V∥
+    // V' = V⊥ - V∥
+    // V' = V - 2 * V∥
+
+    const velocityParallelToSlope = normalVector
+      .copy()
+      .mult(this.velocity.copy().dot(normalVector) / normalVector.copy().magSq());
+    this.velocity.sub(velocityParallelToSlope.mult(2));
   }
 }
